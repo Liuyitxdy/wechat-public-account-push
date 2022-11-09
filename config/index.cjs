de115d0da93a0154dcb2885a8991467a4a8bbbce
Liuyitxdy <95669425+Liuyitxdy@users.noreply.github.com>
2022-11-09 12:48:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'reset',
+      name: '.美女不洗头',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oFJHL540jxfdUscHKAKPExUJ3-CA',
+      id: 'oFJHL53SF1pXj-E2ZCBVr8vbGC3Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'SfXml8cfySh-tsGwGuIUJKl4XBDm_rb8UnPcMND3ueI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '小赵',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oFJHL540jxfdUscHKAKPExUJ3-CA',
+      id: 'oFJHL55w977B3Z5VQwDp_w7LX15E',
     }
   ],
 
